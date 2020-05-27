6af510dd18101e5f3d90255155fcaa7e51abf8dd
Aditya Dwi Rachmantya <aditya.rachmantya@dana.id>
2020-05-27 11:39:00 +0700

    Add required fragment and activity.

---
+++ app/src/main/java/com/adr/imagemachine/MachineDataDetailActivity.java
@@ -0,0 +1,14 @@
+package com.adr.imagemachine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MachineDataDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_machine_data_detail);
+    }
+}
