53847ee3deb743534d34bd33ac20c066b615eaff
Aditya Dwi Rachmantya <aditya.rachmantya@dana.id>
2020-05-27 20:06:38 +0700

    Add permission request.

--- app/src/main/java/com/adr/imagemachine/MainActivity.java
+++ app/src/main/java/com/adr/imagemachine/MainActivity.java
@@ -1,9 +1,12 @@
 package com.adr.imagemachine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.adr.imagemachine.adapters.MachineDataVPAdapter;
@@ -16,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.CAMERA ) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions( this, new String[]{android.Manifest.permission.CAMERA}, 0);
+        }
 
         ViewPager viewPager = findViewById(R.id.machine_data_view_pager);
         fragmentPagerAdapter = new MachineDataVPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
